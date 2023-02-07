6591c6d8cf03f98a97c0478e134b59ec21280d49
guying125 <2430713057@qq.com>
2023-02-07 13:00:06 +0800

    配置vueConfigJs

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 practice
 
 1. 前端练习
+   - i18n 国际化
+   - pdf预览
+   - vue3新特性
+   
--- practice-static/vue.config.js
+++ practice-static/vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
+  publicPath: "./",
+  devServer: {
+    // 前端启动端口
+    port: 8090,
+    https: false,
+    open: false,
+    proxy: {
+      "/": {
+        target: "http://localhost:8080"
+      }
+    },
+    headers: {
+      // 允许应用跨域
+      "Access-Control-Allow-Origin": "*"
+    }
+  },
   transpileDependencies: true
 })
